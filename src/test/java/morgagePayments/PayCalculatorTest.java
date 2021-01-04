dd24252292a0993d4f33886570b02458b03701c8
Mihai Sorinescu <67548283+mihai080976@users.noreply.github.com>
2021-01-05 00:51:41 +0200

    Initial Commit :Basic structure for maven project

---
+++ src/test/java/morgagePayments/PayCalculatorTest.java
@@ -0,0 +1,21 @@
+package morgagePayments;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PayCalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void power() {
+    }
+}
